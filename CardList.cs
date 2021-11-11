12e5ecd08bdeb9912fa67a4abe76b410b6390dfe
Mateusz Borycki <mateusz.borycki@huuugegames.com>
2021-11-11 10:23:31 +0100

    Added additional helpers.

---
+++ CardList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class CardBaseList
+{
+    static protected Dictionary<string, CardLogicBase> _baseCards = new Dictionary<string, CardLogicBase>();
+    public static CardLogicBase GetBaseLogic(string id)
+    {
+        _baseCards.TryGetValue(id, out var ret);
+        return ret;
+    }
+}
+public class GenericCardList<CardLogicChild> : CardBaseList where CardLogicChild : CardLogicBase 
+{
+    static Dictionary<string, CardLogicChild> _cards = new Dictionary<string, CardLogicChild>();
+
+    public static CardLogicChild GetLogic(string id)
+    {
+        _cards.TryGetValue(id, out var ret);
+        return ret;
+    }
+
+    static public Dictionary<string, CardLogicChild> GetAllCards()
+    {
+        return _cards;
+    }
+
+    static protected void LoadCardsByReflection()
+    {
+        Type parentType = typeof(CardLogicChild);
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        Type[] types = assembly.GetTypes();
+        IEnumerable<Type> subclasses = types.Where(t => t.IsSubclassOf(parentType));
+
+        foreach (Type type in subclasses)
+        {
+            var logic = (CardLogicChild)Activator.CreateInstance(type);
+            if (string.IsNullOrEmpty(logic.id)) continue;
+            AddCard(logic);
+        }
+    }
+
+    static protected void AddCard(CardLogicChild logic)
+    {
+        Debug.Assert(logic.id != null, $"Card id is not set");
+
+        if (Application.isPlaying && _cards.ContainsKey(logic.id))
+        {
+            Debug.LogWarning($"{logic.id} already added to list!");
+        }
+
+        AddCard(logic.id, logic);
+    }
+
+    static protected void AddCards(IEnumerable<CardLogicChild> logic)
+    {
+        foreach (var card in logic)
+        {
+            AddCard(card);
+        }
+    }
+
+    static protected void AddCard(string name, CardLogicChild logic)
+    {
+        _cards[name] = logic;
+        _baseCards[name] = logic;
+    }
+}
---
+++ CardLogic.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CardLogicBase
+{
+    public string id;
+    public HashSet<string> Tags = null;
+
+    public bool HasTag(string tag)
+    {
+        if (Tags == null) return false;
+        return Tags.Contains(tag);
+    }
+
+    virtual public IEnumerable<Targetable> AvailableTargets(Card card)
+    {
+        return new List<Targetable> { GameState.Current.Board };
+    }
+
+    virtual public object[] TextVariables(Card card)
+    {
+        return new object[] { };
+    }
+
+    // -----------------------------------------
+    // Utils
+    // -----------------------------------------
+
+
+    virtual public void AddedToDeck(Card card, Card deck, bool addedNow)
+    {
+        AddListenersAfterCardInPlay(card, deck);
+    }
+
+
+    // helpers for adding listeners for active effects
+    virtual public void AddListenersAfterCardInPlay(Card card, Card deck)
+    {
+
+    }
+
+    virtual public void RemoveListenersAfterCardRemovedFromPlay(Card card)
+    {
+        CleanupForCard(card);
+    }
+
+
+    static protected void AddListener<T>(Card c, UnityEvent<T> e, UnityAction<T> call)
+    {
+        e.AddListener(call);
+        AddCleanup(c, () => e.RemoveListener(call));
+    }
+
+    static protected void AddListener(Card c, UnityEvent e, UnityAction call)
+    {
+        e.AddListener(call);
+        AddCleanup(c, () => e.RemoveListener(call));
+    }
+
+
+    static Dictionary<Card, List<Action>> _cardCleanupQueue = new Dictionary<Card, List<Action>>();
+    static void AddCleanup(Card c, Action a)
+    {
+        List<Action> actions = null;
+        if (!_cardCleanupQueue.TryGetValue(c, out actions))
+        {
+            actions = new List<Action>();
+            _cardCleanupQueue[c] = actions;
+        }
+        actions.Add(a);
+    }
+
+    static protected void CleanupForCard(Card c)
+    {
+        List<Action> actions = null;
+        if (!_cardCleanupQueue.TryGetValue(c, out actions)) return;
+        foreach (var a in actions)
+        {
+            a.Invoke();
+        }
+
+        _cardCleanupQueue.Remove(c);
+    }
+}
